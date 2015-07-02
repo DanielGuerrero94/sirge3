d7e89527ae9cd281b1391a90ca12d23afca7dadf
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2015-07-02 12:34:05 -0300

    Coloreando el IMPORTANTE

---
+++ IMPORTANTE.sql
@@ -0,0 +1,26 @@
+ESQUEMAS X : TERMINADO
+/*[R][X]	aplicacion_fondos_old */ -> 'PASAR A OTRA BDD [sirge_pato]'
+--[R][X]	beneficiarios*/
+--[G][X]	comprobantes
+/*[R][X]	comprobantes_old */-> 'PASAR A OTRA BDD [sirge_pato]'
+--[R][X]	compromiso_anual
+[G][ ]	consultas
+--[R][X]	ddjj
+--[R][X]	efectores
+--[G][X]	fondos
+--[R][X]	geo
+--[R][X]	indec
+--[R][X]	indicadores
+[R][ ]	logs
+--[G][X]	mobile
+[R][ ]	osp
+--[G][X]	prestaciones
+/*[G][X]	prestaciones_old */ -> 'PASAR A OTRA BDD [sirge_pato]'
+[R][ ]	profe
+--[R][X]	pss
+--[G][X]	public
+--[G][X]	puco
+--[G][X]	sistema
+[R][ ]	sss
+--[G][X] 	tmp
+--[R][X] 	trazadoras
