e75e97bf42483776a001a50a75e8e4ccbe64517f
Gustavo D. Hekel <gustavo.hekel@gmail.com>
2015-07-06 20:42:28 -0300

    06 jul 2015

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -1,13 +1,13 @@
 <?php
 namespace App\Http\Controllers;
-use App\Classes\Usuario;
+use App\Classes\Area;
 use App\Http\Controllers\Controller;
 
 class UsuarioController extends Controller {
 
 	public function test() {
 
-		$e = Usuario::find(1)->area->nombre;
+		$e = Area::find(1)->usuarios;
 		echo '<pre>', print_r($e), '</pre>';
 	}
 
