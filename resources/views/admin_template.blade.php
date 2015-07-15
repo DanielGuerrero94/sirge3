2c6ae0c7e049fee65d4a15e76564096eb2244f93
Gustavo D. Hekel <gustavo.hekel@gmail.com>
2015-07-14 21:58:35 -0300

    Resources

--- resources/views/admin_template.blade.php
+++ resources/views/admin_template.blade.php
@@ -67,7 +67,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 <!-- REQUIRED JS SCRIPTS -->
 
-<!-- jQuery 2.1.3 -->
+<!-- jQuery 2.1.4 -->
 <script src="{{ asset ("/bower_components/admin-lte/plugins/jQuery/jQuery-2.1.4.min.js") }}"></script>
 <!-- Bootstrap 3.3.2 JS -->
 <script src="{{ asset ("/bower_components/admin-lte/bootstrap/js/bootstrap.min.js") }}" type="text/javascript"></script>
