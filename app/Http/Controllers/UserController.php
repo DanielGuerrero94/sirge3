714843a78ad5980da58832696757cdf89f05fb50
gustavo <gustavo.hekel@gmail.com>
2015-08-04 18:44:06 -0300

    usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,14 +24,14 @@ class UserController extends Controller
      * @return view
      */
     public function index(Request $r){
-    	$usuarios = Usuario::with('menu' , 'area' , 'provincia' , 'conexiones')->paginate(2);;
+    	$usuarios = Usuario::with('menu' , 'area' , 'provincia' , 'conexiones')->paginate(15);;
     	$usuarios->setPath('usuarios');
     	//echo '<pre>',print_r($r->query()),'</pre>';
     	$data = [
     		'page_title' => 'ABM Usuarios',
     		'usuarios' => $usuarios
     	];
-    	
+
     	if (sizeof($r->query())){
     		return view('admin.usuarios-table' , $data);
     	} else {
