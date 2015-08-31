a1ad85f048e6281d114306e5b4f4c91179fa3cf4
gustavo <gustavo.hekel@gmail.com>
2015-08-31 18:34:45 -0300

    Requests

--- app/Http/Controllers/SolicitudController.php
+++ app/Http/Controllers/SolicitudController.php
@@ -27,7 +27,7 @@ class SolicitudController extends Controller
      * @return void
      */
     public function __construct(){
-    	$this->middleware('auth');
+    	$this->middleware('auth' , ['except' => 'finalizarSolicitud']);
     }
 
     /**
