4bfd8834b8282cbfa44240d1d6babb53e6cb67aa
gustavo <gustavo.hekel@gmail.com>
2015-09-11 16:27:48 -0300

    Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -172,4 +172,48 @@ Route::get('php-info' , function(){
 	phpinfo();
 });
 
+Route::get('prestaciones/34142469' , function(){
+	$json = 
+	"{
+		'nombre' : 'Anakin',
+		'apellido' : 'Skywalker',
+		'dni' : 34142469,
+		'prestaciones' : {[
+			1 : {
+				'codigo' : 'CTC001A97',
+				'nombre' : 'Control periódico de salud',
+				'fecha' : 30/12/2013,
+				'lugar' : {
+					'nombre' : 'HOSPITAL POSADAS',
+					'SIISA' : '10065682000343'
+				},
+				'datos reportables' : {
+					'peso' : 69,
+					'talla' : 174,
+					'tensión arterial' : '70/120'
+				},
+				'observaciones' : 'Presenta quemaduras de 3er. grado'
+			},
+			2 : {
+				'codigo' : 'AP0003U89',
+				'nombre' : 'Internación',
+				'fecha' : 30/12/2013,
+				'lugar' : {
+					'nombre' : 'HOSPITAL POSADAS',
+					'SIISA' : '10065682000343'
+				},
+				'datos reportables' : {},
+				'observaciones' : 'Presenta fiebre extrema'
+			}
+		]},
+		'domicilio' : {
+			'calle' : 'Av. Nazca',
+			'altura' : '1450',
+			'piso' : '4',
+			'departamento' : 'F'
+		}
+	}";
+	return '<pre>' . $json . '</pre>';
+});
+
 
