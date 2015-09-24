1873a86ac2dd6722e562cda1dcf94cd7091b087a
Gustavo D. Hekel <gustavo.hekel@gmail.com>
2015-09-24 02:14:44 -0300

    Dw dia 1

--- app/Http/Controllers/EstadisticasController.php
+++ app/Http/Controllers/EstadisticasController.php
@@ -81,38 +81,83 @@ class EstadisticasController extends Controller
     public function getGafico2(){
         $i = 0;
         $regiones = Ceb001::where('periodo' , 201501)
-                        ->join('sistema.provincias as p' , 'c001.id_provincia' , '=' , 'p.id_provincia')
-                        ->join('sistema.regiones as r' , 'p.id_region' , '=' , 'r.id_region')
+                        ->join('geo.provincias as p' , 'c001.id_provincia' , '=' , 'p.id_provincia')
+                        ->join('geo.regiones as r' , 'p.id_region' , '=' , 'r.id_region')
                         ->select('r.id_region' , 'r.nombre' , DB::raw('sum(cantidad_facturada) as cantidad'))
                         ->groupBy('r.id_region')
                         ->groupBy('r.nombre')
                         ->get();
         foreach ($regiones as $key => $region){
             $data[$i]['color'] = $this->alter_brightness('#0F467F' , $key * 35);
-            $data[$i]['id'] = (string)$key;
+            $data[$i]['id'] = (string)$region->id_region;
             $data[$i]['name'] = $region->nombre;
             $data[$i]['value'] = (int)$region->cantidad;
             $i++;
         }
 
-        $provincias = Ceb001::where('periodo' , 201501)
-                        ->where('r.id_region' , 1)
-                        ->join('sistema.provincias as p' , 'c001.id_provincia' , '=' , 'p.id_provincia')
-                        ->join('sistema.regiones as r' , 'p.id_region' , '=' , 'r.id_region')
-                        ->select('r.id_region' , 'p.id_provincia' , 'p.nombre' , DB::raw('sum(cantidad_facturada) as cantidad'))
-                        ->groupBy('r.id_region')
-                        ->groupBy('p.id_provincia')
-                        ->groupBy('p.nombre')
-                        ->get();
-        foreach ($provincias as $key => $provincia){
-            $data[$i]['id'] = "1_" . (string)$key;
-            $data[$i]['name'] = $provincia->nombre;
-            $data[$i]['parent'] = 1;
-            $data[$i]['value'] = (int)$provincia->cantidad;
-            $i++;
+        for ($j = 0 ; $j <= 5 ; $j ++){
+            $provincias = Ceb001::where('periodo' , 201501)
+                            ->where('r.id_region' , $j)
+                            ->join('geo.provincias as p' , 'c001.id_provincia' , '=' , 'p.id_provincia')
+                            ->join('geo.regiones as r' , 'p.id_region' , '=' , 'r.id_region')
+                            ->select('r.id_region' , 'p.id_provincia' , 'p.nombre' , DB::raw('sum(cantidad_facturada) as cantidad'))
+                            ->groupBy('r.id_region')
+                            ->groupBy('p.id_provincia')
+                            ->groupBy('p.nombre')
+                            ->get();
+            foreach ($provincias as $key => $provincia){
+                $data[$i]['id'] = $provincia->id_region . "_" . $provincia->id_provincia;
+                $data[$i]['name'] = $provincia->nombre;
+                $data[$i]['parent'] = (string)$provincia->id_region;
+                $data[$i]['value'] = (int)$provincia->cantidad;
+                $i++;
+            }
         }
 
+        for ($k = 1 ; $k <= 24 ; $k ++){
+            $matriz_aux = [];
+            $codigos = Ceb001::where('periodo' , 201501)
+                            ->where('p.id_provincia' , str_pad($k , 2 , '0' , STR_PAD_LEFT))
+                            ->join('geo.provincias as p' , 'c001.id_provincia' , '=' , 'p.id_provincia')
+                            ->join('geo.regiones as r' , 'p.id_region' , '=' , 'r.id_region')
+                            ->select('r.id_region' , 'p.id_provincia' , 'c001.codigo_prestacion' , DB::raw('sum(cantidad_facturada) as cantidad'))
+                            ->groupBy('r.id_region')
+                            ->groupBy('p.id_provincia')
+                            ->groupBy('c001.codigo_prestacion')
+                            ->orderBy(DB::raw('sum(cantidad_facturada)') , 'desc')
+                            ->take(15)
+                            ->get();
+            foreach ($codigos as $key => $codigo){
+                $matriz_aux[] = $codigo->codigo_prestacion;
+                $data[$i]['id'] = $codigo->id_region . "_" . $codigo->id_provincia . "_" . $codigo->codigo_prestacion;
+                $data[$i]['name'] = $codigo->codigo_prestacion;
+                $data[$i]['parent'] = $codigo->id_region . "_" . $codigo->id_provincia;
+                $data[$i]['value'] = (int)$codigo->cantidad;
+                $i++;   
+            }
 
+            for ($l = 0 ; $l < count($matriz_aux) ; $l ++){
+                $grupos = Ceb001::where('periodo' , 201501)
+                                ->where('p.id_provincia' , str_pad($k , 2 , '0' , STR_PAD_LEFT))
+                                ->where('codigo_prestacion' , $matriz_aux[$l])
+                                ->join('geo.provincias as p' , 'c001.id_provincia' , '=' , 'p.id_provincia')
+                                ->join('geo.regiones as r' , 'p.id_region' , '=' , 'r.id_region')
+                                ->join('pss.grupos_etarios as g' , 'c001.grupo_etario' , '=' , 'g.sigla')
+                                ->select('r.id_region' , 'p.id_provincia' , 'c001.codigo_prestacion' , 'g.descripcion' , DB::raw('sum(cantidad_facturada) as cantidad'))
+                                ->groupBy('r.id_region')
+                                ->groupBy('p.id_provincia')
+                                ->groupBy('c001.codigo_prestacion')
+                                ->groupBy('g.descripcion')
+                                ->get();
+                foreach ($grupos as $key => $grupo){
+                    $data[$i]['id'] = $grupo->id_region . "_" . $grupo->id_provincia . "_" . $grupo->codigo_prestacion . "_" . $grupo->grupo_etario;
+                    $data[$i]['name'] = $grupo->descripcion;
+                    $data[$i]['parent'] = $grupo->id_region . "_" . $grupo->id_provincia . "_" . $grupo->codigo_prestacion;
+                    $data[$i]['value'] = (int)$grupo->cantidad;
+                    $i++;   
+                }
+            }
+        }
         return response()->json($data);
     }
 
--- resources/views/estadisticas/graficos/graficos/2.blade.php
+++ resources/views/estadisticas/graficos/graficos/2.blade.php
@@ -24,79 +24,36 @@
 		});
 	});
 
-    var data = $.ajax({
-        method : 'get',
-        url : 'grafico-2',
-        async : false,
-        success : function(data){
-            return data;
-        }
-    });
-
-    data = JSON.parse(data.responseText);
-    console.log(data);
-
-
-    $('#container').highcharts({
-        series: [{
-            type: "treemap",
-            layoutAlgorithm: 'squarified',
-            allowDrillToNode: true,
-            dataLabels: {
-                enabled: false
-            },
-            levelIsConstant: false,
-            levels: [{
-                level: 1,
+    $.get('grafico-2' , function(data){
+        $('#container').highcharts({
+            series: [{
+                type: "treemap",
+                layoutAlgorithm: 'squarified',
+                allowDrillToNode: true,
                 dataLabels: {
-                    enabled: true
+                    enabled: false
                 },
-                borderWidth: 3
+                levelIsConstant: false,
+                levels: [{
+                    level: 1,
+                    dataLabels: {
+                        enabled: true
+                    },
+                    borderWidth: 3
+                }],
+                data : data,
+                tooltip: {
+                    pointFormat: "{point.value}"
+                },
+                turboThreshold : 5000
             }],
-            data : data,
-            tooltip: {
-                pointFormat: "{point.value}"
+            subtitle: {
+                text: 'Fuente: <a href="http://apps.who.int/gho/data/node.main.12?lang=en">OMS</a>.'
+            },
+            title : {
+            	text : 'Facturación CEB'
             }
-            /*[
-            {
-                id : "1",
-                name : 'CENTRO',
-                value : 20,
-                color: "#FF00FF"
-            },
-            {
-                id : "2",
-                name : 'cuyo',
-                value : 20,
-                color: "#FF00FF"
-            },
-            {
-                id : "3",
-                name : 'noa',
-                value : 20,
-                color: "#FF00FF"
-            },
-            {
-                id : "6",
-                name : 'BUENOS AIRES',
-                parent : "1"
-            },
-            {
-                id : "7",
-                name : 'algo',
-                value : 20,
-                parent : "6"
-            }
-            ]
-            */
-        }],
-        subtitle: {
-            text: 'Fuente: <a href="http://apps.who.int/gho/data/node.main.12?lang=en">OMS</a>.'
-        },
-        title : {
-        	text : ''
-        }
-        
+        });
     });
 </script>
 @endsection
