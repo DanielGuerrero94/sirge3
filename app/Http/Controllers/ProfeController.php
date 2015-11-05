1277275c8f0de28a5fa933c8db29516177b57291
Gustavo D. Hekel <gustavo.hekel@gmail.com>
2015-11-05 16:35:51 -0300

    Profe

--- app/Http/Controllers/ProfeController.php
+++ app/Http/Controllers/ProfeController.php
@@ -241,8 +241,6 @@ class ProfeController extends Controller
 
 		if (sizeof($bulk)){
 			Super::insert($bulk);
-			unset($bulk);
-			$bulk = [];
 		}
 
 		$this->actualizaLote($lote , $this->_resumen);
