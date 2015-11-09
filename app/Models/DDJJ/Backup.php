ecb5a3170239b923a975b8fd283dd8930c62a4d2
Gustavo D. Hekel <gustavo.hekel@gmail.com>
2015-11-09 10:42:03 -0300

    Domingo

---
+++ app/Models/DDJJ/Backup.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\DDJJ;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Backup extends Model
+{
+    /**
+	 * The table associated with the model
+	 *
+	 * @var string
+	 */
+	protected $table = 'ddjj.sirge';
+
+	/**
+	 * Primary key asociated with the table.
+	 *
+	 * @var string
+	 */
+	protected $primaryKey = 'id_impresion';
+
+	/**
+	 * Indicates if the model should be timestamped.
+	 *
+	 * @var bool
+	 */
+	public $timestamps = false;
+}
---
+++ app/Models/DDJJ/DOIU9.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\DDJJ;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DOIU9 extends Model
+{
+    //
+}
