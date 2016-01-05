a0944a9187f2f65af2c138468308ea5a0b2ecd6d
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-01-05 15:54:19 -0300

    cambios en ProvinciasSeeder

--- database/seeds/SistemaProvinciasSeeder.php
+++ database/seeds/SistemaProvinciasSeeder.php
@@ -13,7 +13,7 @@ class SistemaProvinciasSeeder extends Seeder {
 		\DB::statement(" INSERT INTO geo.provincias(id_provincia,descripcion,id_region,latlong)
 						 (
 							SELECT *
-							FROM dblink('dbname=sirge host=192.6.0.66 user=postgres password=110678',
+							FROM dblink('dbname=sirge2 host=192.6.0.66 user=postgres password=110678',
 							    'SELECT id_entidad, descripcion, id_region, (SELECT ll FROM geo.provincias gp WHERE p.id_entidad = gp.id_provincia) as ll
 								    FROM sistema.entidades p where id_entidad::integer < 25')
 							    AS sirge_provincias(id_provincia character varying(2), descripcion character varying(100), id_region integer, ll character varying(100))
