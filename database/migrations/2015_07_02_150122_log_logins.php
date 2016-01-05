81c3d272d512a90a8d62faa13d852c37f114f9c8
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-01-05 10:04:02 -0300

    cambios en la creacion de tablas

--- database/migrations/2015_07_02_150122_log_logins.php
+++ database/migrations/2015_07_02_150122_log_logins.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableLogLogins extends Migration {
+class LogLogins extends Migration {
 	/**
 	 * Run the migrations.
 	 *
