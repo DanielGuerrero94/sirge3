48a376b0af02bc27000e927126a314075512f84c
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-01-05 16:22:48 -0300

    cambios en continuando con Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,12 +15,12 @@ class DatabaseSeeder extends Seeder {
 		$this->call('SistemaProvinciasSeeder');
 		$this->call('SistemaAreasSeeder');
 		$this->call('PssDiagnosticosSeeder');
-		/*$this->call('PssCodigosSeeder');
+		$this->call('PssCodigosSeeder');
 		$this->call('SistemaClasesDocumento');
 		$this->call('PssGruposEtarios');
 		$this->call('SistemaSexos');
 		$this->call('SistemaTipoDocumento');
-		$this->call('BeneficiariosBeneficiarios');
+		/*$this->call('BeneficiariosBeneficiarios');
 		$this->call('SistemaMenues');
 		$this->call('SistemaTipoEntidad');
 		$this->call('SistemaEntidades');		
