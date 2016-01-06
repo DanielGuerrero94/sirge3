146da3b8f80206991c87bcf24cfc8c61f6b37d1c
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-01-06 14:17:36 -0300

    continuando con Seeders

--- database/seeds/GeoLocalidades.php
+++ database/seeds/GeoLocalidades.php
@@ -14,7 +14,7 @@ class GeoLocalidades extends Seeder {
 	SELECT *
 	FROM dblink('dbname=sirge3 host=192.6.0.118 user=postgres password=LatinoSandwich007',
 	    'SELECT id,id_provincia,id_departamento,id_localidad,nombre_localidad
-		    FROM efectores.localidades')
+		    FROM geo.localidades')
 	    AS migracion( id integer,id_provincia character(2),
   id_departamento character(3),
   id_localidad character(3),
