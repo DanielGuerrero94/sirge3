c04d5a3d41a84ba99ad65a0a4363025d2367eb60
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-01-06 13:14:07 -0300

    continuando con Seeders

--- database/seeds/SistemaUsuarios.php
+++ database/seeds/SistemaUsuarios.php
@@ -9,11 +9,11 @@ class SistemaUsuarios extends Seeder {
 	 * @return void
 	 */
 	public function run() {
-		\DB::statement(" INSERT INTO sistema.usuarios(id_usuario,usuario,password,nombre,email,activo,id_area,id_menu,telefono,id_entidad,last_login,created_at,updated_at)
+		\DB::statement(" INSERT INTO sistema.usuarios(id_usuario,usuario,password,nombre,email,activo,id_area,id_menu,telefono,id_provincia,id_entidad,last_login,created_at,updated_at)
  (
 	SELECT *
 	FROM dblink('dbname=sirge host=192.6.0.118 user=postgres password=PN2012$',
-	    'SELECT id_usuario,usuario,password,descripcion as nombre,email,activo,id_area,id_menu,null as telefono,id_entidad::integer,now() as last_login,now() as created_at, now() as updated_at
+	    'SELECT id_usuario,usuario,password,descripcion as nombre,email,activo,id_area,id_menu,null as telefono,case id_entidad when ''25'' then ''01'' else id_entidad end as id_provincia,case id_entidad when ''25'' then 1 else 2 end as id_entidad,now() as last_login,now() as created_at, now() as updated_at
 		    FROM sistema.usuarios')
 	    AS migracion( id_usuario integer,
 			  usuario character varying(50),
@@ -24,6 +24,7 @@ class SistemaUsuarios extends Seeder {
 			  id_area integer,
 			  id_menu integer,
 			  telefono character varying(20),
+			  id_provincia character(2),
 			  id_entidad integer,
 			  last_login timestamp(0) without time zone,
 			  created_at timestamp(0) without time zone,
