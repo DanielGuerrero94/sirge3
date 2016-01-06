1e940d9c5cf9c7294ef79ac5b8c774ff425e6347
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-01-06 18:30:15 -0300

    continuando con Seeders

--- database/seeds/BeneficiariosBeneficiariosGeografico.php
+++ database/seeds/BeneficiariosBeneficiariosGeografico.php
@@ -13,24 +13,25 @@ class BeneficiariosBeneficiariosGeografico extends Seeder
     {
         \DB::statement(" INSERT INTO beneficiarios.geografico(clave_beneficiario,calle,numero,manzana,piso,departamento,calle_1,calle_2,barrio,municipio,id_departamento,id_localidad,id_provincia,codigo_postal)
 	(
-		SELECT *
+		SELECT clave_beneficiario_n,calle_n,numero_n,manzana_n,piso_n,departamento_n,calle_1_n,calle_2_n,barrio_n,municipio_n,(SELECT id FROM geo.departamentos WHERE id_departamento = id_departamento_n AND id_provincia = id_provincia_n) as id_departamento ,(SELECT id FROM geo.localidades WHERE id_departamento = id_departamento_n AND id_provincia = id_provincia_n AND id_localidad = id_localidad_n) as id_localidad,id_provincia_n,codigo_postal_n
+		
 		FROM dblink('dbname=sirge host=192.6.0.118 user=postgres password=PN2012\$',
 		    'SELECT clave_beneficiario,calle,numero,manzana,piso,departamento,calle_1,calle_2,barrio,municipio,id_departamento,id_localidad,id_provincia,codigo_postal
 			    FROM beneficiarios.beneficiarios_geografico limit 10000')
-		    AS migracion(clave_beneficiario character varying(16),
-				  calle character varying(100),
-				  numero character varying(10),
-				  manzana character varying(5),
-				  piso character varying(3),
-				  departamento character varying(3),
-				  calle_1 character varying(100),
-				  calle_2 character varying(100),
-				  barrio character varying(100),
-				  municipio character varying(100),
-				  id_departamento character varying(3),
-				  id_localidad character varying(3),
-				  id_provincia character varying(2),
-				  codigo_postal character varying(8))			
+		    AS migracion(clave_beneficiario_n character varying(16),
+				  calle_n character varying(100),
+				  numero_n character varying(10),
+				  manzana_n character varying(5),
+				  piso_n character varying(3),
+				  departamento_n character varying(3),
+				  calle_1_n character varying(100),
+				  calle_2_n character varying(100),
+				  barrio_n character varying(100),
+				  municipio_n character varying(100),
+				  id_departamento_n character varying(3),
+				  id_localidad_n character varying(3),
+				  id_provincia_n character varying(2),
+				  codigo_postal_n character varying(8))			
 	); 	");
     }
 }
