ec2ae95a987b86932b5ded04e2554e1548452e62
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-01-11 11:08:45 -0300

    continuando con Seeders

--- database/seeds/EfectoresEfectoresAddendas.php
+++ database/seeds/EfectoresEfectoresAddendas.php
@@ -11,7 +11,7 @@ class EfectoresEfectoresAddendas extends Seeder
      */
     public function run()
     {
-        \DB::statement(" INSERT INTO efectores.efectores_addendas(id,id_efector,id_addenda,fecha_addenda,created_at,update_at)
+        \DB::statement(" INSERT INTO efectores.efectores_addendas(id,id_efector,id_addenda,fecha_addenda,created_at,updated_at)
 (
 	SELECT *
 	FROM dblink('dbname=sirge host=192.6.0.118 user=postgres password=PN2012\$',
