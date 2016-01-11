53cbbb6fe496b98d4942e13b40032e827e4d3c88
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-01-11 10:16:42 -0300

    continuando con Seeders

--- database/seeds/IndicadoresMetasEfectoresPriorizados.php
+++ database/seeds/IndicadoresMetasEfectoresPriorizados.php
@@ -17,7 +17,7 @@ class IndicadoresMetasEfectoresPriorizados extends Seeder
 		FROM dblink('dbname=sirge2 host=192.6.0.66 user=postgres password=110678',
 		    'SELECT efector,base,c1,c2,c3,indicador
 			FROM
-				indicadores.indicadores_metas_efectores_priorizados;')
+				indicadores.metas_efectores_priorizados;')
 		    AS migracion(efector character varying (14),
 				base integer,
 				c1 integer,
