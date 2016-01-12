03ce4a9fbe367621ef11d06342abf1b29f9e349b
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-01-12 16:29:54 -0300

    Indicadores

--- database/seeds/IndicadoresIndicadoresMedica.php
+++ database/seeds/IndicadoresIndicadoresMedica.php
@@ -11,20 +11,27 @@ class IndicadoresIndicadoresMedica extends Seeder
      */
     public function run()
     {
-        \DB::statement(" INSERT INTO indicadores.indicadores_medica(id_provincia,periodo,codigo_indicador,resultado,id_rango_indicador)
+        \DB::statement(" INSERT INTO indicadores.indicadores_medica(id_provincia,periodo,codigo_indicador,resultado)
 	(
 		SELECT *
 		FROM dblink('dbname=sirge host=192.6.0.118 user=postgres password=PN2012\$',
-		    'SELECT id_provincia,periodo,codigo_indicador,coalesce(resultado,0),id_rango_indicador
-		    (SELECT r.id_rango_indicador FROM indicadores.indicadores_medica_rangos r WHERE i.id_provincia = r.id_provincia AND substring(r.periodo,1,4) = substring(i.periodo,1,4) AND left(i.codigo_indicador,-2) =  r.codigo_indicador) as id_rango_indicador
+		    'SELECT id_provincia,periodo,codigo_indicador,coalesce(resultado,0)		    
 			FROM
 				indicadores.indicadores_medica i;')
 		    AS migracion(id_provincia char(2),
 				periodo integer,
 				codigo_indicador character varying(6),
-				resultado integer,
-				id_rango_indicador integer
+				resultado integer				
 				)
 	); ");
+
+        \DB::statement(" UPDATE indicadores.indicadores_medica SET id_rango_indicador = r.valor 
+						FROM
+							(SELECT id_rango_indicador as valor
+								FROM indicadores.indicadores_medica_rangos r 
+								WHERE id_provincia = r.id_provincia 
+								AND substring(r.periodo::text,1,4) = substring(periodo::text,1,4) 
+								AND left(codigo_indicador,-2) =  r.codigo_indicador
+							) as r ");
     }
 }
