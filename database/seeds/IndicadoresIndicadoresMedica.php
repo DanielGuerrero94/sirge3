43608101bad9558a1c5f74b925f392f2960c21f8
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-01-12 16:03:09 -0300

    Indicadores

--- database/seeds/IndicadoresIndicadoresMedica.php
+++ database/seeds/IndicadoresIndicadoresMedica.php
@@ -15,7 +15,7 @@ class IndicadoresIndicadoresMedica extends Seeder
 	(
 		SELECT *
 		FROM dblink('dbname=sirge host=192.6.0.118 user=postgres password=PN2012\$',
-		    'SELECT id_provincia,periodo,codigo_indicador,coalesce(resultado,0),
+		    'SELECT id_provincia,periodo,codigo_indicador,coalesce(resultado,0),id_rango_indicador
 		    (SELECT r.id_rango_indicador FROM indicadores.indicadores_medica_rangos r WHERE i.id_provincia = r.id_provincia AND substring(r.periodo,1,4) = substring(i.periodo,1,4) AND left(i.codigo_indicador,-2) =  r.codigo_indicador) as id_rango_indicador
 			FROM
 				indicadores.indicadores_medica i;')
