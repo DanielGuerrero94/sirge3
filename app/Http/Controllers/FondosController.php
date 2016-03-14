61d68820860fabf9a146cffe75b6fa6458238c9b
gustavo <gustavo.hekel@gmail.com>
2016-03-14 12:30:56 -0300

    bug fix

--- app/Http/Controllers/FondosController.php
+++ app/Http/Controllers/FondosController.php
@@ -190,7 +190,7 @@ class FondosController extends Controller
 						if ($e->getCode() == 23505){
 							$this->_error['motivos'] = '{"pkey" : ["Registro ya informado"]}';
 						} else {
-							$this->_error['motivos'] = '{"' . $e->getCode() . '" : ["' . $e->getMessage() . '"]}';
+							$this->_error['motivos'] = '{"' . $e->getCode() . '" : ["' . json_encode($e->getMessage()) . '"]}';
 						}
 						Rechazo::insert($this->_error);
 					}
