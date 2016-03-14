6b4ea4c9d679956487b1b5be292b4e6052e25196
Gustavo D. Hekel <gustavo.hekel@gmail.com>
2016-03-14 14:35:47 -0300

    bug fix

--- resources/views/pdf/asignacion.blade.php
+++ resources/views/pdf/asignacion.blade.php
@@ -12,7 +12,7 @@
 <body>
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
-			<img width="700px" src="{{ asset("/dist/img/header-sumar.png") }}">
+			<img src="/var/www/html/sirge3/public/dist/img/header-sumar.png" style="width: 100%;">
 			<div class="box box-danger">
 				<div class="box-header">
 					<h2 class="box-title">Asignación requerimiento Nº: <b>{{ $solicitud->id }}</b></h2>
