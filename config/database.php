68f47ccd4b207fa65caa4f140b5707da0c8dde30
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-03-14 10:25:46 -0300

    Config database fondos

--- config/database.php
+++ config/database.php
@@ -141,6 +141,28 @@ return [
             'schema'   => 'puco',
         ],
 
+        'fondos' => [
+            'driver'   => 'pgsql',
+            'host'     => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'fondos',
+        ],
+
+         'ddjj' => [
+            'driver'   => 'pgsql',
+            'host'     => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'ddjj',
+        ],
+
         'indicadores' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
