a7389f65f294a6d37869a871deacd67c267326f5
gustavo <gustavo.hekel@gmail.com>
2016-03-15 16:40:20 -0300

    bug fix

--- app/Http/Controllers/ComprobantesController.php
+++ app/Http/Controllers/ComprobantesController.php
@@ -154,7 +154,7 @@ class ComprobantesController extends Controller
 				if(count($this->_data) == count($linea)){
 					$comprobante_raw = array_combine($this->_data, $linea);
 
-					if (is_null($comprobante_raw['fecha_debito_bancario'])){
+					if (strlen($comprobante_raw['fecha_debito_bancario']) < 10){
 						$comprobante_raw['fecha_debito_bancario'] = '1900-01-01';
 					}
 
