7a59f37902b2f55dc87992f2360ed880db657336
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-06-09 12:05:14 -0300

    Cambio en la fecha limite para presentar, se estira hasta el 9 temporalmente

--- app/Http/Middleware/Padronok.php
+++ app/Http/Middleware/Padronok.php
@@ -38,7 +38,7 @@ class Padronok
     {        
         
 
-        if(intval(date('d')) <= 7){
+        if(intval(date('d')) <= 9){
             $dt = new \DateTime();
             $dt->modify('-1 month');
             $dt->modify('first day of this month');
