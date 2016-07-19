c3defaab3c89f8635910d78a8dca821d03de8fd3
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-07-19 15:18:14 -0300

    Excepcion ddjj

--- app/Http/Middleware/Padronok.php
+++ app/Http/Middleware/Padronok.php
@@ -38,7 +38,7 @@ class Padronok
     {        
         
 
-        if(intval(date('d')) <= 15){
+        if(intval(date('d')) <= 19){
             $dt = new \DateTime();
             $dt->modify('-1 month');
             $dt->modify('first day of this month');
