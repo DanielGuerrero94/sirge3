c0d123c2acad2fe8a0e7b06ecf0a5da7edc5eb7a
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-09-16 12:52:43 -0300

    Red local y externa agregados en el cierre de solicitudes

--- resources/views/emails/request-cierre.blade.php
+++ resources/views/emails/request-cierre.blade.php
@@ -68,7 +68,9 @@ background-color: #f6f6f6;
           </tr><tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td class="content-wrap" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 20px;" valign="top">
               <table width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
                     Estimado usuario, en el adjunto podrá encontrar información con respecto a la solución a la solicitud ingresada. <br />
-                    <b><a href="sirge.gustavohekel.com.ar/solicitud-final/{{ $id }}/{{ $hash }}">Le solicitamos que nos haga saber que la solución es satisfactoria haciendo click en este enlace.</a></b>
+                    <b>Le solicitamos que nos confirme que la solución es satisfactoria haciendo click uno de los siguientes enlaces según corresponda:</b><br /><br />
+                    <b><a href="200.69.210.3/sirge3/public/solicitud-final/{{ $id }}/{{ $hash }}">Confirmar cierre de solicitud desde Internet</a></b><br /><br />
+                    <b><a href="192.6.0.37/sirge3/public/solicitud-final/{{ $id }}/{{ $hash }}">Confirmar cierre de solicitud desde UEC (para usuarios en red local)</a></b>                    
                   </td>
                 </tr><tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
                     Gracias por utilizar SIRGe Web.
