02b9f34865dbf413aaa1005c4c234f85dffc957e
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-10-02 11:58:54 -0300

    Servicio para obtener los beneficiarios del sirge en renaper

--- app/Http/Controllers/WebServicesController.php
+++ app/Http/Controllers/WebServicesController.php
@@ -184,7 +184,7 @@ class WebServicesController extends Controller
                                   ->where('id_provincia_alta' , '05')
                                   ->where('clase_documento' , 'P')
                                   ->whereNull('i.nrodocumento')
-                                  ->take(10)
+                                  ->take(30000)
                                   ->lists('numero_documento');                 
 
         foreach ($documentos as $key => $documento){
