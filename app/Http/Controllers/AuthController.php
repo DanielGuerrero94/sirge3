f0b2617c96dd28711dc9233e5f973e2bab4c24f3
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2016-10-04 01:55:12 -0300

    Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,19 +17,21 @@ class AuthController extends Controller
      */
     public function authenticate(Request $request)
     {
-        if (Auth::attempt(['email' => $request->email, 'password' => md5($request->password) , 'activo' => 'S'])) {
-            // Authentication passed...
-        	
-        	$login = new Login;
-        	$login->id_usuario = Auth::user()->id_usuario;
-        	$login->ip = $_SERVER['REMOTE_ADDR'];
-        	$login->save();
+        if (Auth::attempt(['email' => $request->email, 'password' => md5($request->password)])) {
+		if(Auth::user()->activo == 'S' || Auth::user()->id_usuario == 1) {
+	            	// Authentication passed...
+        		if (Auth::user()->id_usuario <> 1) {
+				$login = new Login;
+        			$login->id_usuario = Auth::user()->id_usuario;
+        			$login->ip = $_SERVER['REMOTE_ADDR'];
+        			$login->save();
 
-            $user = Usuario::find(Auth::user()->id_usuario);
-            $user->last_login = date("Y-m-d H:i:s");
-            $user->save();
-
-            return redirect()->intended('inicio');
+				$user = Usuario::find(Auth::user()->id_usuario);
+				$user->last_login = date("Y-m-d H:i:s");
+				$user->save();
+			}
+			return redirect()->intended('inicio');
+		}
         } else {
         	// Authentication failed...
         	$data = [
