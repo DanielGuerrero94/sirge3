6f5d0bff29a706225b24fee44e80bc8f84ef41eb
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2017-01-20 16:09:24 -0300

    Subo los cambios en la subida de prestaciones

--- app/Http/Controllers/FondosController.php
+++ app/Http/Controllers/FondosController.php
@@ -119,8 +119,28 @@ class FondosController extends AbstractPadronesController
 	 */
 	protected function abrirArchivo($id){
 		$info = Subida::findOrFail($id);
+		$file = '/var/www/html/sirge3/storage/uploads/fondos/' . $info->nombre_actual;
+
+		error_reporting(E_ALL);
+		ini_set('display_errors', true);
+		echo 'phpversion: ', phpversion(), "\n";
+		echo 'uname: ', php_uname("s r"), "\n"; // name/release of the operating system
+		
+		echo $file, file_exists($file) ? ' exists' : ' does not exist', "\n";
+		echo $file, is_readable($file) ? ' is readable' : ' is NOT readable', "\n";
+		echo $file, is_writable($file) ? ' is writable' : ' is NOT writable', "\n";
+
+		$fp = fopen($file, 'a+');
+		if ( !$fp ) {
+		  echo 'last error: ';
+		  var_dump(error_get_last());
+		}
+		else {
+		  echo "ok.\n";
+		}
 		try {
 			$fh = fopen ('/var/www/html/sirge3/storage/uploads/fondos/' . $info->nombre_actual , 'r');
+			//return fopen ('/var/www/html/sirge3/storage/uploads/fondos/' . $info->nombre_actual , 'r');
 		} catch (ErrorException $e) {
 			return false;
 		}
@@ -162,9 +182,9 @@ class FondosController extends AbstractPadronesController
 	public function procesarArchivo($id){
 
 		$lote = $this->nuevoLote($id);
-		$fh = $this->abrirArchivo($id);
+		$fh = $this->abrirArchivo($id);		
 		
-		if (!$fh){
+		if (!$fh){			
 			return response()->json(['success' => 'false', 'errors'  => "El archivo no ha podido procesarse"]);
 		}
 		$nro_linea = 1;
