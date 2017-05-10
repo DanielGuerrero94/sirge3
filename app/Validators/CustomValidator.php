05b87cba5e180914cf8a2bef829d65c6cbfd7f36
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2017-05-10 15:15:21 -0300

    Custom validator, no se utiliza todavia

--- app/Validators/CustomValidator.php
+++ app/Validators/CustomValidator.php
@@ -2,11 +2,18 @@
 
 namespace App\Validators;
 
-class CustomValidator extends Illuminate\Validation\Validator {
+use Illuminate\Validation\Validator;
+
+class CustomValidator extends Validator {
 
     public function validateNoNumeric($attribute, $value, $parameters)
     {
         return !is_numeric($value);
+    }
+
+    public function validateFullName($attribute, $value, $parameters)
+    {
+        return preg_match('/^[\pL\s\'\,\.']+$/u', $value);
     }
 
 }
