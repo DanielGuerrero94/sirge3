c385cf94a13fee8565335f8afe805f400fa3c9b9
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2017-05-11 15:26:28 -0300

    Pruebo con 10 beneficiarios a cruzar en renaper

--- app/Http/Controllers/WebServicesController.php
+++ app/Http/Controllers/WebServicesController.php
@@ -207,7 +207,7 @@ class WebServicesController extends Controller
 
         $client = $this->create();
 
-        $cantidad = 1000;
+        $cantidad = 10;
 
         $dt = \DateTime::createFromFormat('Ym' , date('Ym'));
         $dt->modify('-2 months');
