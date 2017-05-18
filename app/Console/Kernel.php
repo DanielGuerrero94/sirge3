b68701615b85dd821cb0e83f31759a41a0073f34
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2017-05-18 16:54:43 -0300

    Corro el procesamiento mensual del datawarehouse con los stored procedures

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends ConsoleKernel
         $schedule->call('App\Http\Controllers\EfectoresController@generarTabla')->dailyAt('21:30');
         $schedule->call('App\Http\Controllers\PssController@generarTabla')->dailyAt('22:00');
         $schedule->call('App\Http\Controllers\RechazosController@generarRechazosLotesNuevos')->hourly();
-        $schedule->call('App\Http\Controllers\DatawarehouseController@ejecutarTodas')->cron('35 16 28 * * *');
+        $schedule->call('App\Http\Controllers\DatawarehouseController@ejecutarTodas')->cron('57 16 18 * * *');
                 
         $schedule->call('App\Http\Controllers\WebServicesController@cruzarBeneficiariosConSiisa')->dailyAt('20:02');    
         $schedule->call('App\Http\Controllers\WebServicesController@cruzarBeneficiariosConSiisa')->dailyAt('20:05');
