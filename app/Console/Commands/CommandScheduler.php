7187bba57fb07a010558f87a9eb975bf7efa80e8
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2017-10-25 13:30:15 -0300

    Cambio el tipo de rechazo por el header de trazadoras[A

--- app/Console/Commands/CommandScheduler.php
+++ app/Console/Commands/CommandScheduler.php
@@ -43,41 +43,53 @@ class CommandScheduler extends Command
      * @return mixed
      */
     public function handle()
-    {                                
-        $subida = Subida::where('id_estado',1)->orderBy('fecha_subida')->first();
-        if($subida){
-            $subida->id_estado = 5;
-            $subida->save();
-            if(!Auth::check()){
-                Auth::loginUsingId($subida->id_usuario, true);
+    {
+        
+        $subidas = Subida::where('id_estado', 1)->orderBy('fecha_subida', 'DESC')->get()->pluck('id_subida')->all();
+        $subidas = array_values($subidas);
+
+        while ($subidas) {
+            $popped_subida = array_pop($subidas);
+            if ($popped_subida) {
+                $subida = Subida::find($popped_subida);
+                if ($subida->id_estado != 5) {
+                    $subida->id_estado = 5;
+                    $subida->save();
+                    if (!Auth::check()) {
+                        Auth::loginUsingId($subida->id_usuario, true);
+                    }
+                    try {
+                        switch ($subida->id_padron) {
+                            case 1:
+                                app('App\Http\Controllers\PrestacionesController')->procesarArchivo($subida->id_subida);
+                                break;
+                            case 2:
+                                app('App\Http\Controllers\FondosController')->procesarArchivo($subida->id_subida);
+                                break;
+                            case 3:
+                                app('App\Http\Controllers\ComprobantesController')->procesarArchivo($subida->id_subida);
+                                break;
+                            case 4:
+                                app('App\Http\Controllers\OspController')->procesarArchivo($subida->id_subida);
+                                break;
+                            case 5:
+                                app('App\Http\Controllers\ProfeController')->procesarArchivo($subida->id_subida);
+                                break;
+                            case 6:
+                                app('App\Http\Controllers\SuperController')->procesarArchivo($subida->id_subida);
+                                break;
+                            case 7:
+                                app('App\Http\Controllers\TrazadorasController')->procesarArchivo($subida->id_subida);
+                                break;
+                        }
+                        Auth::logout();
+                        Subida::where('id_subida', $subida->id_subida)->update(['id_estado' => 3]);
+                    } catch (Exception $e) {
+                        var_dump($e->getMessage());
+                        return json_encode($e->getMessage());
+                    }
+                }
             }
-            try {
-                switch ($subida->id_padron) {
-                    case 1:
-                        app('App\Http\Controllers\PrestacionesController')->procesarArchivo($subida->id_subida);
-                        break;
-                    case 2:
-                        app('App\Http\Controllers\FondosController')->procesarArchivo($subida->id_subida);
-                        break;
-                    case 3:
-                        app('App\Http\Controllers\ComprobantesController')->procesarArchivo($subida->id_subida);
-                        break;
-                    case 4:
-                        app('App\Http\Controllers\OspController')->procesarArchivo($subida->id_subida);
-                        break;
-                    case 5:
-                        app('App\Http\Controllers\ProfeController')->procesarArchivo($subida->id_subida);
-                        break;
-                    case 6:
-                        app('App\Http\Controllers\SuperController')->procesarArchivo($subida->id_subida);
-                        break;
-                }            
-                Auth::logout();
-                Subida::where('id_subida',$subida->id_subida)->update(['id_estado' => 3]);                    
-            } catch (Exception $e) {
-                return json_encode($e->getMessage());
-                            
-            }
-        }                
+        }
     }
 }
