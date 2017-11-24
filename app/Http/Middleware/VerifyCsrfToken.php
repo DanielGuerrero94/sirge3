b25f958f8a08d09d3211173f7fde4556b9ddd05f
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2017-11-24 11:24:35 -0300

    Cambios en las Cookies de laravel

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,9 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Cookie;
+use Illuminate\Session\TokenMismatchException;
 
 class VerifyCsrfToken extends BaseVerifier
 {
@@ -14,4 +17,48 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    /**
+     * Determine if the session and input CSRF tokens match.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function tokensMatch($request)
+    {
+        $sessionToken = $request->session()->token();
+
+        $token = $request->input('_token') ?: $request->header('X-CSRF-TOKEN-SIRGE');
+
+        if (! $token && $header = $request->header('X-XSRF-TOKEN-SIRGE')) {
+            $token = $this->encrypter->decrypt($header);
+        }
+
+        if (! is_string($sessionToken) || ! is_string($token)) {
+            return false;
+        }
+
+        return Str::equals($sessionToken, $token);
+    }
+
+    /**
+     * Add the CSRF token to the response cookies.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Response  $response
+     * @return \Illuminate\Http\Response
+     */
+    protected function addCookieToResponse($request, $response)
+    {
+        $config = config('session');
+
+        $response->headers->setCookie(
+            new Cookie(
+                'XSRF-TOKEN-SIRGE', $request->session()->token(), time() + 60 * 120,
+                $config['path'], $config['domain'], $config['secure'], false
+            )
+        );
+
+        return $response;
+    }
 }
