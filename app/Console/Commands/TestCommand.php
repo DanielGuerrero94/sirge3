a7e0fa8f32ce88997b97c9604ce13c43bf9d09dd
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2018-04-23 11:31:15 -0300

    Procesar manualmente el  generadot de rechazos

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -2,53 +2,55 @@
 
 namespace App\Console\Commands;
 
-use DB;
-use Auth;
-use App\Models\Scheduler;
-use App\Models\Subida;
 use Illuminate\Console\Command;
 
-use App\Http\Controllers\Controller;
+class TestCommand extends Command {
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'test:run';
 
-class TestCommand extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'test:run';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Command description';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle() {
+		//app('App\Http\Controllers\WebServicesController')->georeferenciarBeneficiarios();
+		//app('App\Http\Controllers\OspController')->procesarArchivo(12809);
+		//app('App\Http\Controllers\DatawarehouseController')->ejecutarTodas();
+		//app('App\Http\Controllers\LotesController')->alertSubidasMalProcesadas();
+		//app('App\Http\Controllers\DatawarehouseController')->ejecutarTodas();
+		//app('App\Http\Controllers\WebServicesController')->cruzarBeneficiariosConSiisa();
+		app('App\Http\Controllers\RechazosController')->generarRechazosLotesNuevos();
+		//DB::table('test')->insert(['mensaje' => 'Hola cómo te va JAVIER MINSKY?, sos bastante gay!']);
+		/*
+	try {
+	MyShellException::execute('sudo pgloader -v --debug /home/rodrigocadaval/Documents/Desktop/indicadores_de_prueba.load', $out);
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {                
-        app('App\Http\Controllers\OspController')->procesarArchivo(12809);
-        //app('App\Http\Controllers\DatawarehouseController')->ejecutarTodas();
-        //app('App\Http\Controllers\LotesController')->alertSubidasMalProcesadas();        
-        //app('App\Http\Controllers\DatawarehouseController')->ejecutarTodas();        
-        //app('App\Http\Controllers\WebServicesController')->cruzarBeneficiariosConSiisa();
-        //app('App\Http\Controllers\RechazosController')->generarRechazosLotesNuevos();
-        //DB::table('test')->insert(['mensaje' => 'Hola cómo te va JAVIER MINSKY?, sos bastante gay!']);
-    }
+	//system('sudo pgloader -v --debug /home/rodrigocadaval/Documents/Desktop/indicadores_de_prueba.load');
+	} catch (MyShellException $e) {
+	echo "Hubo un error \n";
+	}
+	 */
+
+	}
 }
