e89502c74db91c0da282ffd4d2e21282d4717506
Rodrigo Cadaval <rodri.cadaval@gmail.com>
2018-09-12 15:51:46 -0300

    Agrego a Ari al listado de solicitudes de usuario.

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,93 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+
+use App\Http\Requests\SolicitudUsuarioRequest;
+use App\Models\Area;
+use App\Models\Usuario;
+
 use Illuminate\Http\Request;
 use Mail;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-use App\Http\Requests\SolicitudUsuarioRequest;
+class RegistrationController extends Controller {
+	/**
+	 * Muestra el formulario de registro
+	 *
+	 * @return view
+	 */
+	public function index() {
+		$data = [
+			'areas' => Area::all()
+		];
 
-use App\Models\Usuario;
-use App\Models\Area;
+		return view('registration.main', $data);
+	}
 
+	/**
+	 * Muestra el formulario de registro
+	 * @param request Información del formulario
+	 *
+	 * @return view
+	 */
+	public function register(SolicitudUsuarioRequest $r) {
+		$user                   = new Usuario;
+		$user->nombre           = $r->nombre.' '.$r->apellido;
+		$user->usuario          = $r->email;
+		$user->email            = $r->email;
+		$user->activo           = 'N';
+		$user->id_provincia     = $r->provincia;
+		$user->id_entidad       = $r->sede;
+		$user->id_area          = $r->area;
+		$user->id_menu          = 9;
+		$user->ruta_imagen      = 'default-avatar.png';
+		$user->fecha_nacimiento = $r->fecha_nacimiento;
+		$user->cargo            = $r->cargo;
+		$user->facebook         = $r->fb;
+		$user->twitter          = $r->tw;
+		$user->linkedin         = $r->ln;
+		$user->google           = $r->gp;
+		$user->skype            = $r->skype;
+		$user->telefono         = $r->telefono;
+		$user->password         = bcrypt(md5($r->pass));
 
-class RegistrationController extends Controller
-{
-    /**
-     * Muestra el formulario de registro
-     *
-     * @return view
-     */
-    public function index()
-    {
-        $data = [
-            'areas' => Area::all()
-        ];
+		if ($user->save()) {
+			Mail::send('emails.reminder', ['usuario' => $user], function ($m) use ($user) {
+					$m->from('sirgeweb@sumar.com.ar', 'Programa SUMAR');
+					$m->to('sirgeweb@gmail.com');
+					$m->to('arielplan@gmail.com');
+					$m->to('javier.minsky@gmail.com');
+					$m->to('ldvernarelli@msal.gov.ar');
+					$m->to('gavacca@msal.gov.ar');
+					$m->to('rodrigo.cadaval.sumar@gmail.com');
+					$m->subject('Solicitud de usuario!');
+				});
+			return view('registration.aviso');
+		}
+	}
 
-    	return view('registration.main' , $data);
-    }
+	/**
+	 * Método para el plugin jQuery Validator que busca si no existe un email
+	 * @param request QueryString
+	 *
+	 * @return json
+	 */
+	public function email(Request $r) {
+		$email  = $r->email;
+		$existe = Usuario::where('email', '=', $email)->get();
 
-    /**
-     * Muestra el formulario de registro
-     * @param request Información del formulario
-     * 
-     * @return view
-     */
-    public function register(SolicitudUsuarioRequest $r)
-    {
-    	$user = new Usuario;
-        $user->nombre = $r->nombre . ' ' . $r->apellido;
-        $user->usuario = $r->email;
-        $user->email = $r->email;
-        $user->activo = 'N';
-        $user->id_provincia = $r->provincia;
-        $user->id_entidad = $r->sede;
-        $user->id_area = $r->area;
-        $user->id_menu = 9;
-        $user->ruta_imagen = 'default-avatar.png';
-        $user->fecha_nacimiento = $r->fecha_nacimiento;
-        $user->cargo = $r->cargo;
-        $user->facebook = $r->fb;
-        $user->twitter = $r->tw;
-        $user->linkedin = $r->ln;
-        $user->google = $r->gp;
-        $user->skype = $r->skype;
-        $user->telefono = $r->telefono;
-        $user->password = bcrypt(md5($r->pass));
-        
-        if ($user->save()){
-            Mail::send('emails.reminder', ['usuario' => $user], function ($m) use ($user) {
-                $m->from('sirgeweb@sumar.com.ar', 'Programa SUMAR');                
-                $m->to('sirgeweb@gmail.com');
-                $m->to('javier.minsky@gmail.com');  
-                $m->to('ldvernarelli@msal.gov.ar');  
-                $m->to('gavacca@msal.gov.ar');                
-                $m->to('rodrigo.cadaval.sumar@gmail.com');
-                $m->subject('Solicitud de usuario!');
-            });
-            return view('registration.aviso');
-        }
-    }
-
-    /**
-     * Método para el plugin jQuery Validator que busca si no existe un email
-     * @param request QueryString
-     *
-     * @return json
-     */
-    public function email(Request $r)
-    {
-        $email = $r->email;
-        $existe = Usuario::where('email' , '=' , $email)->get();
-
-        if (! count($existe)){
-            $json = true;
-        } else {
-            $json = false;
-        }
-        return json_encode($json);
-    }
-
+		if (!count($existe)) {
+			$json = true;
+		} else {
+			$json = false;
+		}
+		return json_encode($json);
+	}
 
 }
