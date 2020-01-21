53e0cc71f88245c3b6d21b024df68fe363640c19
Daniel <daniel.guerrero.94@gmail.com>
2020-01-21 13:18:31 -0300

    fix definicion de las vistas

--- database/psql/mv_efectores_completo.definition.sql
+++ database/psql/mv_efectores_completo.definition.sql
@@ -1,53 +1,6 @@
-                                      Materialized view "efectores.mv_efectores_completo"
-            Column            |          Type          | Collation | Nullable | Default | Storage  | Stats target | Description 
-------------------------------+------------------------+-----------+----------+---------+----------+--------------+-------------
- cuie                         | character(6)           |           |          |         | extended |              | 
- siisa                        | character(14)          |           |          |         | extended |              | 
- nombre_efector               | character varying(200) |           |          |         | extended |              | 
- domicilio                    | character varying(500) |           |          |         | extended |              | 
- codigo_postal                | character varying(8)   |           |          |         | extended |              | 
- denominacion_legal           | character varying(200) |           |          |         | extended |              | 
- id_tipo_efector              | integer                |           |          |         | plain    |              | 
- sigla_tipo                   | character(3)           |           |          |         | extended |              | 
- tipo_efector                 | character varying(50)  |           |          |         | extended |              | 
- rural                        | character(1)           |           |          |         | extended |              | 
- cics                         | character varying(1)   |           |          |         | extended |              | 
- id_categorizacion            | integer                |           |          |         | plain    |              | 
- sigla_categorizacion         | character(6)           |           |          |         | extended |              | 
- categorizacion               | character varying(100) |           |          |         | extended |              | 
- dependencia_sanitaria        | character varying(200) |           |          |         | extended |              | 
- compromiso_gestion           | character(1)           |           |          |         | extended |              | 
- hcd                          | character(1)           |           |          |         | extended |              | 
- nombre_hce                   | character varying(50)  |           |          |         | extended |              | 
- id_provincia                 | character(2)           |           |          |         | extended |              | 
- nombre_provincia             | character varying(100) |           |          |         | extended |              | 
- indec_departamento           | character(3)           |           |          |         | extended |              | 
- departamento                 | character varying(200) |           |          |         | extended |              | 
- indec_localidad              | character varying(5)   |           |          |         | extended |              | 
- localidad                    | character varying(200) |           |          |         | extended |              | 
- ciudad                       | character varying(200) |           |          |         | extended |              | 
- latitud                      | double precision       |           |          |         | plain    |              | 
- longitud                     | double precision       |           |          |         | plain    |              | 
- internet                     | character(1)           |           |          |         | extended |              | 
- factura_descentralizada      | character(1)           |           |          |         | extended |              | 
- factura_online               | character(1)           |           |          |         | extended |              | 
- numero_compromiso            | character varying(50)  |           |          |         | extended |              | 
- firmante_compromiso          | character varying(200) |           |          |         | extended |              | 
- fecha_suscripcion_compromiso | date                   |           |          |         | plain    |              | 
- fecha_inicio_compromiso      | date                   |           |          |         | plain    |              | 
- fecha_fin_compromiso         | date                   |           |          |         | plain    |              | 
- pago_indirecto               | character varying(1)   |           |          |         | extended |              | 
- numero_convenio              | character varying(50)  |           |          |         | extended |              | 
- firmante_convenio            | character varying(200) |           |          |         | extended |              | 
- nombre_tercer_administrador  | character varying(200) |           |          |         | extended |              | 
- codigo_tercer_administrador  | character varying(50)  |           |          |         | extended |              | 
- fecha_suscripcion_convenio   | date                   |           |          |         | plain    |              | 
- fecha_inicio_convenio        | date                   |           |          |         | plain    |              | 
- fecha_fin_convenio           | date                   |           |          |         | plain    |              | 
- email                        | character varying(200) |           |          |         | extended |              | 
- telefono                     | character varying(200) |           |          |         | extended |              | 
-View definition:
- SELECT v_efectores_completo.cuie,
+drop materialized view efectores.mv_efectores_completo;
+create materialized view efectores.mv_efectores_completo as ( 
+SELECT v_efectores_completo.cuie,
     v_efectores_completo.siisa,
     v_efectores_completo.nombre AS nombre_efector,
     v_efectores_completo.domicilio,
@@ -92,5 +45,4 @@ View definition:
     v_efectores_completo.convenio_fecha_fin AS fecha_fin_convenio,
     v_efectores_completo.email,
     v_efectores_completo.telefono
-   FROM efectores.v_efectores_completo;
-
+   FROM efectores.v_efectores_completo);
--- database/psql/v_efectores_completo.definition.sql
+++ database/psql/v_efectores_completo.definition.sql
@@ -1,87 +1,4 @@
-                                          View "efectores.v_efectores_completo"
-            Column             |              Type              | Collation | Nullable | Default | Storage  | Description 
--------------------------------+--------------------------------+-----------+----------+---------+----------+-------------
- id_efector                    | integer                        |           |          |         | plain    | 
- cuie                          | character(6)                   |           |          |         | extended | 
- siisa                         | character(14)                  |           |          |         | extended | 
- nombre                        | character varying(200)         |           |          |         | extended | 
- domicilio                     | character varying(500)         |           |          |         | extended | 
- codigo_postal                 | character varying(8)           |           |          |         | extended | 
- denominacion_legal            | character varying(200)         |           |          |         | extended | 
- id_tipo_efector               | integer                        |           |          |         | plain    | 
- rural                         | character(1)                   |           |          |         | extended | 
- cics                          | character varying(1)           |           |          |         | extended | 
- id_categorizacion             | integer                        |           |          |         | plain    | 
- id_dependencia_administrativa | integer                        |           |          |         | plain    | 
- dependencia_sanitaria         | character varying(200)         |           |          |         | extended | 
- integrante                    | character(1)                   |           |          |         | extended | 
- compromiso_gestion            | character(1)                   |           |          |         | extended | 
- priorizado                    | character(1)                   |           |          |         | extended | 
- ppac                          | character(1)                   |           |          |         | extended | 
- id_estado                     | integer                        |           |          |         | plain    | 
- created_at                    | timestamp(0) without time zone |           |          |         | plain    | 
- updated_at                    | timestamp(0) without time zone |           |          |         | plain    | 
- hcd                           | character(1)                   |           |          |         | extended | 
- id_sistema_hcd                | smallint                       |           |          |         | plain    | 
- sistema_hcd                   | character varying(50)          |           |          |         | extended | 
- dg_id_efector                 | integer                        |           |          |         | plain    | 
- id_provincia                  | character(2)                   |           |          |         | extended | 
- nombre_provincia              | character varying(100)         |           |          |         | extended | 
- id_departamento               | smallint                       |           |          |         | plain    | 
- id_localidad                  | smallint                       |           |          |         | plain    | 
- ciudad                        | character varying(200)         |           |          |         | extended | 
- latitud                       | double precision               |           |          |         | plain    | 
- longitud                      | double precision               |           |          |         | plain    | 
- indec_departamento            | character(3)                   |           |          |         | extended | 
- nombre_departamento           | character varying(200)         |           |          |         | extended | 
- loc_indec_departamento        | character(3)                   |           |          |         | extended | 
- indec_localidad               | character varying(5)           |           |          |         | extended | 
- nombre_localidad              | character varying(200)         |           |          |         | extended | 
- desc_id_efector               | integer                        |           |          |         | plain    | 
- internet                      | character(1)                   |           |          |         | extended | 
- factura_descentralizada       | character(1)                   |           |          |         | extended | 
- factura_on_line               | character(1)                   |           |          |         | extended | 
- id_compromiso                 | integer                        |           |          |         | plain    | 
- comp_id_efector               | integer                        |           |          |         | plain    | 
- numero_compromiso             | character varying(50)          |           |          |         | extended | 
- firmante                      | character varying(200)         |           |          |         | extended | 
- fecha_suscripcion             | date                           |           |          |         | plain    | 
- fecha_inicio                  | date                           |           |          |         | plain    | 
- fecha_fin                     | date                           |           |          |         | plain    | 
- pago_indirecto                | character varying(1)           |           |          |         | extended | 
- id_convenio                   | integer                        |           |          |         | plain    | 
- conv_id_efector               | integer                        |           |          |         | plain    | 
- numero_convenio               | character varying(50)          |           |          |         | extended | 
- convenio_firmante             | character varying(200)         |           |          |         | extended | 
- nombre_tercer_administrador   | character varying(200)         |           |          |         | extended | 
- codigo_tercer_administrador   | character varying(50)          |           |          |         | extended | 
- convenio_fecha_suscripcion    | date                           |           |          |         | plain    | 
- convenio_fecha_inicio         | date                           |           |          |         | plain    | 
- convenio_fecha_fin            | date                           |           |          |         | plain    | 
- neo_siisa                     | character(14)                  |           |          |         | extended | 
- id_categoria                  | integer                        |           |          |         | plain    | 
- neo_id                        | integer                        |           |          |         | plain    | 
- obs_siisa                     | character(14)                  |           |          |         | extended | 
- obs_id_categoria              | integer                        |           |          |         | plain    | 
- obs_id                        | integer                        |           |          |         | plain    | 
- ppac_id_efector               | integer                        |           |          |         | plain    | 
- addenda_perinatal             | character(1)                   |           |          |         | extended | 
- fecha_addenda_perinatal       | date                           |           |          |         | plain    | 
- perinatal_ac                  | character(1)                   |           |          |         | extended | 
- add_id                        | integer                        |           |          |         | plain    | 
- add_id_efector                | integer                        |           |          |         | plain    | 
- id_addenda                    | integer                        |           |          |         | plain    | 
- fecha_addenda                 | date                           |           |          |         | plain    | 
- tipo_id_tipo_efector          | integer                        |           |          |         | plain    | 
- sigla                         | character(3)                   |           |          |         | extended | 
- tipo_efector_descripcion      | character varying(50)          |           |          |         | extended | 
- tipo_cat_id_categorizacion    | integer                        |           |          |         | plain    | 
- cat_sigla                     | character(6)                   |           |          |         | extended | 
- tipo_cat_descripcion          | character varying(100)         |           |          |         | extended | 
- prio_fecha                    | date                           |           |          |         | plain    | 
- email                         | character varying(200)         |           |          |         | extended | 
- telefono                      | character varying(200)         |           |          |         | extended | 
-View definition:
+create or replace view efectores.v_efectores_completo as (
  SELECT e.id_efector,
     e.cuie,
     e.siisa,
@@ -185,5 +102,4 @@ View definition:
      LEFT JOIN efectores.tipo_categorizacion cat ON e.id_categorizacion = cat.id_categorizacion
      LEFT JOIN geo.provincias prov ON dg.id_provincia = prov.id_provincia
      LEFT JOIN hcd.sistemas hcd ON e.id_sistema_hcd = hcd.id_sistema
-  WHERE e.id_estado = 1;
-
+  WHERE e.id_estado = 1);
